9371d71213e10bb80fe38354319cf3e87b102d4c
tronghuan98hd <30307373+tronghuan98hd@users.noreply.github.com>
2019-05-24 16:17:50 +0700

    insert new form

---
+++ DLPK/DLPK/Gui/frm_Register.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace DLPK.Gui
+{
+    partial class frm_Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbRegister = new System.Windows.Forms.GroupBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbCompany = new System.Windows.Forms.Label();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.lbAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.gbRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbRegister
+            // 
+            this.gbRegister.BackColor = System.Drawing.Color.White;
+            this.gbRegister.Controls.Add(this.btnRegister);
+            this.gbRegister.Controls.Add(this.txtDescription);
+            this.gbRegister.Controls.Add(this.lbDescription);
+            this.gbRegister.Controls.Add(this.txtAddress);
+            this.gbRegister.Controls.Add(this.lbAddress);
+            this.gbRegister.Controls.Add(this.txtCompany);
+            this.gbRegister.Controls.Add(this.lbCompany);
+            this.gbRegister.Controls.Add(this.textBox3);
+            this.gbRegister.Controls.Add(this.lbEmail);
+            this.gbRegister.Controls.Add(this.txtPhone);
+            this.gbRegister.Controls.Add(this.lbPhone);
+            this.gbRegister.Controls.Add(this.textBox1);
+            this.gbRegister.Controls.Add(this.lbName);
+            this.gbRegister.Location = new System.Drawing.Point(197, 12);
+            this.gbRegister.Name = "gbRegister";
+            this.gbRegister.Size = new System.Drawing.Size(415, 426);
+            this.gbRegister.TabIndex = 0;
+            this.gbRegister.TabStop = false;
+            this.gbRegister.Text = "Đăng kí";
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbName.Location = new System.Drawing.Point(22, 40);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(43, 15);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "Họ tên";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(25, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "(Bắt buộc)";
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.AutoSize = true;
+            this.lbPhone.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPhone.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbPhone.Location = new System.Drawing.Point(22, 95);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(77, 15);
+            this.lbPhone.TabIndex = 0;
+            this.lbPhone.Text = "Số điện thoại";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.ForeColor = System.Drawing.Color.Gray;
+            this.txtPhone.Location = new System.Drawing.Point(25, 113);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(173, 22);
+            this.txtPhone.TabIndex = 1;
+            this.txtPhone.Text = "(Bắt buộc)";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmail.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbEmail.Location = new System.Drawing.Point(22, 145);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(35, 15);
+            this.lbEmail.TabIndex = 0;
+            this.lbEmail.Text = "Email";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Gray;
+            this.textBox3.Location = new System.Drawing.Point(25, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 22);
+            this.textBox3.TabIndex = 1;
+            // 
+            // lbCompany
+            // 
+            this.lbCompany.AutoSize = true;
+            this.lbCompany.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCompany.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbCompany.Location = new System.Drawing.Point(22, 198);
+            this.lbCompany.Name = "lbCompany";
+            this.lbCompany.Size = new System.Drawing.Size(83, 15);
+            this.lbCompany.TabIndex = 0;
+            this.lbCompany.Text = "Tên cơ sở y tế";
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCompany.ForeColor = System.Drawing.Color.Gray;
+            this.txtCompany.Location = new System.Drawing.Point(25, 216);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(372, 22);
+            this.txtCompany.TabIndex = 1;
+            this.txtCompany.Text = "Bệnh viện, phòng khám, tổ chức, công ty";
+            // 
+            // lbAddress
+            // 
+            this.lbAddress.AutoSize = true;
+            this.lbAddress.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAddress.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbAddress.Location = new System.Drawing.Point(22, 247);
+            this.lbAddress.Name = "lbAddress";
+            this.lbAddress.Size = new System.Drawing.Size(44, 15);
+            this.lbAddress.TabIndex = 0;
+            this.lbAddress.Text = "Địa chỉ";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.ForeColor = System.Drawing.Color.Gray;
+            this.txtAddress.Location = new System.Drawing.Point(25, 265);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(372, 22);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescription.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbDescription.Location = new System.Drawing.Point(22, 298);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(57, 15);
+            this.lbDescription.TabIndex = 0;
+            this.lbDescription.Text = "Nội dung";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.ForeColor = System.Drawing.Color.Gray;
+            this.txtDescription.Location = new System.Drawing.Point(25, 316);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(372, 22);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(25, 364);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(101, 37);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Đăng kí";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // frm_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbRegister);
+            this.Name = "frm_Register";
+            this.Text = "frm_Register";
+            this.gbRegister.ResumeLayout(false);
+            this.gbRegister.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbRegister;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lbAddress;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.Label lbCompany;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lbPhone;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbName;
+    }
+}

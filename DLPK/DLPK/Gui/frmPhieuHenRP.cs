d75416069243cc7d166eee50ebb12ea3be67bbd5
hoangphonmta <48667749+hoangphonmta@users.noreply.github.com>
2019-05-28 12:05:25 +0700

    chốt

--- DLPK/DLPK/Gui/frmPhieuHenRP.cs
+++ DLPK/DLPK/Gui/frmPhieuHenRP.cs
@@ -21,24 +21,32 @@ namespace DLPK.Gui
         private void button1_Click(object sender, EventArgs e)
         {
             string TenBN = txtTenBN.Text;
-            int SDT = Convert.ToInt32(txtSDT.Text);
-            DataTable data = phieuHen.PhieuHenChiTiet(TenBN,SDT);
-            try
+            if (txtSDT.Text =="" || txtTenBN.Text=="")
             {
-                DateTime NgayKham = Convert.ToDateTime(data.Rows[0]["NgayKham"].ToString());
-                lbTenBN.Text = data.Rows[0]["TenBN"].ToString();
-                lbGioiTinh.Text = data.Rows[0]["GioiTinh"].ToString();
-                lbNamSinh.Text = data.Rows[0]["NamSinh"].ToString();
-                lbSDT.Text = data.Rows[0]["SDT"].ToString();
-                lbTenBS.Text = data.Rows[0]["TenBS"].ToString();
-                lbPhongKham.Text = data.Rows[0]["TenPK"].ToString();
-                lbDiaChi.Text = data.Rows[0]["DiaChi"].ToString();
-                lbNgayKham.Text = NgayKham.ToString("dd/MM/yyyy");
-                lbGioKham.Text = data.Rows[0]["ThoiGian"].ToString();
-                lbGiaKham.Text = data.Rows[0]["GiaTien"].ToString();
+                MessageBox.Show("vui lòng nhập thông tin", "thông báo");
             }
-            catch (Exception)
-            { MessageBox.Show("Không tồn tại dữ liệu bạn vừa nhập,vui lòng kiểm tra lại", "Thông báo"); }
+            else
+            {
+                int SDT = Convert.ToInt32(txtSDT.Text);
+                DataTable data = phieuHen.PhieuHenChiTiet(TenBN, SDT);
+                try
+                {
+                    DateTime NgayKham = Convert.ToDateTime(data.Rows[0]["NgayKham"].ToString());
+                    lbTenBN.Text = data.Rows[0]["TenBN"].ToString();
+                    lbGioiTinh.Text = data.Rows[0]["GioiTinh"].ToString();
+                    lbNamSinh.Text = data.Rows[0]["NamSinh"].ToString();
+                    lbSDT.Text = data.Rows[0]["SDT"].ToString();
+                    lbTenBS.Text = data.Rows[0]["TenBS"].ToString();
+                    lbPhongKham.Text = data.Rows[0]["TenPK"].ToString();
+                    lbDiaChi.Text = data.Rows[0]["DiaChi"].ToString();
+                    lbNgayKham.Text = NgayKham.ToString("dd/MM/yyyy");
+                    lbGioKham.Text = data.Rows[0]["ThoiGian"].ToString();
+                    lbGiaKham.Text = data.Rows[0]["GiaTien"].ToString();
+                }
+                catch (Exception)
+                { MessageBox.Show("Không tồn tại dữ liệu bạn vừa nhập,vui lòng kiểm tra lại", "Thông báo"); }
+            }
+            
             
         }
 
